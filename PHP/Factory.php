f3f273fc81dca6124ff144a62e030dddc4738154
eadsimone <eadesimone@gmail.com>
2014-01-15 17:29:38 -0200

    Update Factory.php

--- PHP/Factory.php
+++ PHP/Factory.php
@@ -1,4 +1,14 @@
 <?php
+/*
+*
+*An interface called IUser defines what a user object should do. The implementation of IUser is called User, and a factory class called UserFactory creates IUser objects. This relationship is shown as UML in Figure 1.
+Figure 1. The factory class and its related IUser interface and user class
+The factory class and its related IUser interface and user class
+If you run this code on the command line using the php interpreter, you get this result:
+% php factory1.php 
+Jack
+%
+*/
 interface IUser
 {
   function getName();
